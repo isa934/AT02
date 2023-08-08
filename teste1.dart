1f30714db0d05bea98a737b9fb3f90d40153c45e
Isabelle Vitoria <Isavitoria381@gmail.com>
2023-08-08 20:19:39 -0300

    Métodos gatters e setters Implementados

--- teste1.dart
+++ teste1.dart
@@ -1,6 +1,20 @@
 class Teste {
-  String estudante;
-  double nota;
+  String _estudante;
+  double _nota;
 
-  Teste(this.estudante, this.nota);
+  Teste(this._estudante, this._nota);
+
+  String get estudante => _estudante;
+  set estudante(String novoEstudante) => _estudante = novoEstudante;
+
+  double get nota => _nota;
+  set nota(double novaNota) => _nota = novaNota;
+}
+
+void main() {
+  Teste teste1 = Teste('Luiz', 7.0);
+  Teste teste2 = Teste('Alice', 10);
+
+  print('Estudante: ${teste1.estudante}, Nota: ${teste1.nota}');
+  print('Estudante: ${teste2.estudante}, Nota: ${teste2.nota}');
 }
